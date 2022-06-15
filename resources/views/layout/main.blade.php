280b7b9c5390e89bb601cf2bb60f8a6f4667b9f8
nuidea77 <edashpurew@gmail.com>
2022-06-15 15:48:35 +0800

    6.15d

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,13 +4,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>АЗИ-ФАРМА ХХК</title>
-   <link rel="icon" type="image/webp" href="{{asset('assets/img/favicon.png')}}">
-  <link rel="preload" as="style" href="{{asset('assets/css/plugins.css')}}">
-  <link rel="preload" as="style" href="{{asset('assets/css/style.css')}}">
-  <link rel="preload" as="style" href="{{asset('assets/css/fonts/dm.css')}}" >
-  <link rel="preload" as="style" href="{{asset('assets/css/fonts/urbanist.css')}}" >
-  <link rel="preload" as="style" href="{{asset('assets/css/fonts/thicccboi.css')}}" >
-  <link rel="preload" as="style" href="{{asset('assets/css/app.css')}}" >
+   <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
+  <link  rel="stylesheet" href="{{asset('assets/css/plugins.css')}}">
+  <link  rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+  <link  rel="stylesheet" href="{{asset('assets/css/fonts/dm.css')}}" >
+  <link  rel="stylesheet" href="{{asset('assets/css/fonts/urbanist.css')}}" >
+  <link  rel="stylesheet" href="{{asset('assets/css/fonts/thicccboi.css')}}" >
+  <link  rel="stylesheet" href="{{asset('assets/css/app.css')}}" >
 </head>
 <body>
     <div class="content-wrapper">
@@ -24,11 +24,11 @@
       <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
     </svg>
   </div>
-  <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
-  <script async src="{{asset('assets/js/plugins.js')}}"></script>
-  <script async src="{{asset('assets/js/theme.js')}}"></script>
-  <script async src="{{asset('assets/js/app.js')}}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+  <script src="{{asset('assets/js/plugins.js')}}"></script>
+  <script src="{{asset('assets/js/theme.js')}}"></script>
+  <script src="{{asset('assets/js/app.js')}}"></script>
 </body>
 
 </html>
